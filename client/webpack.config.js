241eed0b3a41162534005a6a1a7a6cd22b1c1b66
LilNewday <lilnewday@gmail.com>
2023-04-19 22:11:13 -0400

    plugins and webpacks successfully filed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates an HTML file that includes the bundled JS files
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
+      // Generates the service worker that includes the bundled JS files
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Generates a manifest file with metadata and icons
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript that works even when offline!',
+        background_color: '#ae00ff',
+        theme_color: '#ae00ff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
+    // Define loaders to use with Webpack
     module: {
       rules: [
-        
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Babel loader to use ES5
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread',
+              '@babel/transform-runtime'
+              ],
+            },
+          },
+        },
       ],
     },
   };
